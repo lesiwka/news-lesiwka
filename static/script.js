6b0a43e0b31afd27ab6088cb2a4eb220d0256f9f
Mystic-Mirage <mm@m10e.net>
2023-06-14 16:31:51 +0300

    Smooth scrolling to a shown article

--- static/script.js
+++ static/script.js
@@ -1,8 +1,17 @@
-document.querySelectorAll(".accordion-item").forEach((el) => {
+document.querySelectorAll(".accordion-item").forEach((el, _, arr) => {
+  el.addEventListener("show.bs.collapse", () => {
+    let offset = el.offsetTop;
+    for (const e of arr) {
+      if (e === el) break;
+      offset -= e.lastElementChild.offsetHeight;
+    }
+
+    window.scroll({behavior: "smooth", left: 0, top: offset - 60});
+    return true;
+  });
+
   el.addEventListener("shown.bs.collapse", () => {
-    window.scroll({
-      top: el.offsetTop - 60,
-      behavior: "smooth",
-    });
+    window.scroll({behavior: "smooth", left: 0, top: el.offsetTop - 60});
+    return true;
   });
 });

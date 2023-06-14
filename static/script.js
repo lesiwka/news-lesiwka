50dbbc38c0de06b25e6ab75476d688493466477e
Mystic-Mirage <mm@m10e.net>
2023-06-14 23:45:02 +0300

    Rework smoothing algorighm

--- static/script.js
+++ static/script.js
@@ -1,17 +1,23 @@
 document.querySelectorAll(".accordion-item").forEach((el, _, arr) => {
-  el.addEventListener("show.bs.collapse", () => {
-    let offset = el.offsetTop;
-    for (const e of arr) {
-      if (e === el) break;
-      offset -= e.lastElementChild.offsetHeight;
-    }
+  let animationFrameId;
 
-    window.scroll({behavior: "smooth", left: 0, top: offset - 60});
-    return true;
+  el.addEventListener("show.bs.collapse", () => {
+    let ratio = 1;
+
+    animationFrameId = window.requestAnimationFrame(function step() {
+      let offset = el.offsetTop - 60;
+      if (offset >= window.scrollY) {
+        offset -= (offset - window.scrollY) / ratio;
+      }
+      ratio *= 1.1;
+
+      window.scroll(0, offset);
+
+      animationFrameId = window.requestAnimationFrame(step);
+    });
   });
 
   el.addEventListener("shown.bs.collapse", () => {
-    window.scroll({behavior: "smooth", left: 0, top: el.offsetTop - 60});
-    return true;
+    window.cancelAnimationFrame(animationFrameId);
   });
 });

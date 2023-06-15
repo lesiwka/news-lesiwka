e08b0071e0f3ddda035c10ad69e28f3ff8f58d1b
Mystic-Mirage <mm@m10e.net>
2023-06-15 14:09:35 +0300

    Another one smoothing algorithm

--- static/script.js
+++ static/script.js
@@ -1,24 +1,10 @@
-document.querySelectorAll(".accordion-item").forEach((el, _, arr) => {
-  let animationFrameId;
-
+document.querySelectorAll(".accordion-item").forEach((el) => {
   el.addEventListener("show.bs.collapse", () => {
-    let ratio = 1;
-
-    animationFrameId = window.requestAnimationFrame(function step() {
-      let offset = el.offsetTop - 60;
-      if (offset >= window.scrollY) {
-        offset -= (offset - window.scrollY) / ratio;
-      }
-      ratio *= 1.1;
-
-      window.scroll(0, offset);
-
-      animationFrameId = window.requestAnimationFrame(step);
+    let diff = el.offsetTop - window.scrollY - 60;
+    window.requestAnimationFrame(function step() {
+      diff = Math.trunc(diff * .8);
+      window.scroll(0, el.offsetTop - 60 - diff);
+      if (diff) window.requestAnimationFrame(step);
     });
-  });
-
-  el.addEventListener("shown.bs.collapse", () => {
-    window.cancelAnimationFrame(animationFrameId);
-    window.scroll({behavior: "smooth", left: 0, top: el.offsetTop - 60});
   });
 });

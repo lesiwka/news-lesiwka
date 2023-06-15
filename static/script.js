861889e0b33ebfefd067877794eb7673a6363f21
Mystic-Mirage <mm@m10e.net>
2023-06-15 17:54:19 +0300

    Slow down an animation a bit

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ document.querySelectorAll(".accordion-item").forEach((el) => {
   el.addEventListener("show.bs.collapse", () => {
     let diff = el.offsetTop - window.scrollY - 60;
     window.requestAnimationFrame(function step() {
-      diff = Math.trunc(diff * .8);
+      diff = Math.trunc(diff * .85);
       window.scroll(0, el.offsetTop - 60 - diff);
       if (diff) window.requestAnimationFrame(step);
     });

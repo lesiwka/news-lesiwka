efe1a9a499be1d4cf149eb11838c820809235086
Mystic-Mirage <mm@m10e.net>
2023-06-19 20:06:13 +0300

    Add a title to the stats page

--- routes/stats.py
+++ routes/stats.py
@@ -1,8 +1,11 @@
+from flask import request
+
 from cache import cache
 
 
 def stats():
     return (
+        f"<title>{request.path}</title>\n"
         "<pre>\n"
         + "\n".join(f"{k}: {v}" for k, v in cache.stats().items())
         + "\n</pre>\n"

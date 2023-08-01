3015d1b9a2e98e5927cf7b39de14cbeb338ad7ff
Mystic-Mirage <mm@m10e.net>
2023-08-01 20:04:30 +0300

    Fix storage removing in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 wget https://novyny.lesiwka.com/_data -O articles.json
 gcloud app deploy app.yaml --version current
 # shellcheck disable=SC2046
-gcloud storage rm -r $(gcloud storage buckets list --uri | rev | cut -d / -f 1 | rev | sed 's|^|gs://|')
+gcloud storage rm -r $(gcloud storage buckets list | grep storage_url | cut -d " " -f 2)

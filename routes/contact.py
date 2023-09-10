d9f0d39896f3d00e198e10b7fd643cbd7de8a9c5
Mystic-Mirage <mm@m10e.net>
2023-09-10 19:13:03 +0300

    Show a contact information on a dedicated page

--- routes/contact.py
+++ routes/contact.py
@@ -1,9 +1,12 @@
 import os
 
-from flask import redirect
+from flask import make_response, redirect, render_template, request
 
 CONTACT = os.environ["CONTACT"]
 
 
 def contact():
-    return redirect(CONTACT)
+    if not request.user_agent.string.startswith("NovynyApp/"):
+        return redirect("/")
+
+    return make_response(render_template("contact.html", contact=CONTACT))

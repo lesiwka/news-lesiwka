ce474532b9d59dc3381a2133ca077c103c2799bd
Mystic-Mirage <mm@m10e.net>
2023-10-22 01:49:32 +0300

    Add `collapse` function to call it from the application

--- static/script.js
+++ static/script.js
@@ -1,10 +1,22 @@
-document.querySelectorAll(".accordion-item").forEach((el) => {
-  el.addEventListener("show.bs.collapse", () => {
-    let diff = el.offsetTop - window.scrollY - 60;
-    window.requestAnimationFrame(function step() {
-      diff = Math.trunc(diff * .85);
-      window.scroll(0, el.offsetTop - 60 - diff);
-      if (diff) window.requestAnimationFrame(step);
-    });
+function scroll2(el) {
+  let diff = el.offsetTop - window.scrollY - 60;
+  window.requestAnimationFrame(function step() {
+    diff = Math.trunc(diff * .85);
+    window.scroll(0, el.offsetTop - 60 - diff);
+    if (diff) window.requestAnimationFrame(step);
   });
+}
+
+document.querySelectorAll(".accordion-item").forEach((el) => {
+  el.addEventListener("show.bs.collapse", () => scroll2(el));
 });
+
+function collapse() {
+  const el = document.querySelector(".accordion-collapse.collapse.show");
+  if (el) {
+    bootstrap.Collapse.getInstance(el).hide();
+    scroll2(el.parentElement);
+    return true;
+  }
+  return false;
+}

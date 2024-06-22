bc0e354270472bf89564bf44ef053d6b64605a75
Mystic-Mirage <mm@m10e.net>
2024-06-22 13:30:24 +0300

    Add hotkeys for the next/previous article (left/right arrow keys)

--- static/script.js
+++ static/script.js
@@ -20,3 +20,66 @@ function collapse() {
   }
   return false;
 }
+
+function nextArticle() {
+  const elements = document.querySelectorAll(".accordion-collapse.collapse");
+  if (elements.length > 0) {
+    const currentEl = document.querySelector(".accordion-collapse.collapse.show, .accordion-collapse.collapsing");
+
+    let nextEl;
+
+    if (currentEl) {
+      for (const [idx, el] of elements.entries()) {
+        if (el === currentEl) {
+          nextEl = elements[idx + 1];
+          bootstrap.Collapse.getInstance(currentEl).hide();
+          break;
+        }
+      }
+    } else {
+      nextEl = elements[0];
+    }
+
+    if (nextEl) {
+      scroll2(nextEl.parentElement);
+      bootstrap.Collapse.getOrCreateInstance(nextEl).show();
+    }
+  }
+}
+
+function prevArticle() {
+  const elements = document.querySelectorAll(".accordion-collapse.collapse");
+  if (elements.length > 0) {
+    const currentEl = document.querySelector(".accordion-collapse.collapse.show, .accordion-collapse.collapsing");
+
+    let prevEl;
+
+    if (currentEl) {
+      for (const [idx, el] of elements.entries()) {
+        if (el === currentEl) {
+          prevEl = elements[idx - 1];
+          bootstrap.Collapse.getInstance(currentEl).hide();
+          break;
+        }
+      }
+    } else {
+      prevEl = elements[elements.length - 1];
+    }
+
+    if (prevEl) {
+      scroll2(prevEl.parentElement);
+      bootstrap.Collapse.getOrCreateInstance(prevEl).show();
+    }
+  }
+}
+
+addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+        prevArticle();
+        break;
+    case "ArrowRight":
+        nextArticle();
+        break;
+  }
+});
